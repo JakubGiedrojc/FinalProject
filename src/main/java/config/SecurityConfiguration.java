74194821c14b8e1d9178412ffc68a1f35c9d117b
Jakub <jakubgiedrojc@gmail.com>
2021-08-21 15:47:49 +0200

    h2 & securityconfiguration 3

--- src/main/java/config/SecurityConfiguration.java
+++ src/main/java/config/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package utils;
+package config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
