f606a5b1fc8be3f096e32407c681bfcdcdf03413
Aikanysh Beksultanova <77458655+Aikanysh91@users.noreply.github.com>
2021-08-28 06:44:32 +0200

    Revert "Aika"

--- src/main/java/com/example/demo/repositories/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.entities.Address;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AddressRepository extends CrudRepository<Address, Integer> {
-}
