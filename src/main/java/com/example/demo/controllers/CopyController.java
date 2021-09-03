43c1a063fff2fe3b577f6d62bcc911931cc1edbf
Aikanysh Beksultanova <aikanyshbeksultanova@gmail.com>
2021-09-03 21:49:11 +0200

    add CopyController.

---
+++ src/main/java/com/example/demo/controllers/CopyController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controllers;
+
+import com.example.demo.entities.Copy;
+import com.example.demo.exceptions.MovieWithThisIdIsNotExistException;
+import com.example.demo.services.AddCopyToCatalogue;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("copy")
+public class CopyController {
+    @Autowired
+    AddCopyToCatalogue addCopyToCatalogue;
+
+    @PostMapping("add/copy")
+    public Copy add (@RequestBody Copy copy) throws MovieWithThisIdIsNotExistException {
+        return addCopyToCatalogue.add(copy);
+    }
+}
